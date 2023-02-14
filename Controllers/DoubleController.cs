8f3634580bcb17890007550e6225983155105fa0
engelsruiz09 <jaruizc@correo.url.edu.gt>
2023-02-14 10:30:12 -0600

    la plantilla

---
+++ Controllers/DoubleController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using LAB01_ED1_G.Models;
+using LAB01_ED1_G.Models.Data;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace LAB01_ED1_G.Controllers
+{
+    public class DoubleController : Controller
+    {
+        public static int i = 0;
+        public static string log = "";
+        Stopwatch stopWatch = new Stopwatch();
+        public static string SName = "";
+        public static string SLName = "";
+        public static decimal? SPay = 0;
+        public static string SClub = "";
+        public static int Id = 0;
+        public static jugador IDFinder = new jugador();
+        public static string PayFinder = "";
+
+        private IWebHostEnvironment Environment;
+
+        public DoubleController(IWebHostEnvironment _environment)
+        {
+            Environment = _environment;
+        }
+        public IActionResult Index()
+        {
+            return View(Singleton.Instance1.PlayerDList);
+        }
+
+        [HttpPost]
+        public ActionResult Index(IFormFile postedFile)
+        {
+            stopWatch.Reset();
+            stopWatch.Start();
+            string Club = "", LName = "", Name = "", Position = "";
+            Decimal Salary = 0, Compensation = 0;
+            if (postedFile != null)
+            {
+                string path = Path.Combine(this.Environment.WebRootPath, "Uploads");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                string fileName = Path.GetFileName(postedFile.FileName);
+                string filePath = Path.Combine(path, fileName);
+                using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+                string csvData = System.IO.File.ReadAllText(filePath);
+                bool firstRow = true;
+                foreach (string row in csvData.Split('\n'))
+                {
+                    if (!string.IsNullOrEmpty(row))
+                    {
+                        if (!string.IsNullOrEmpty(row))
+                        {
+                            if (firstRow)
+                            {
+                                firstRow = false;
+                            }
+                            else
+                            {
+                                int y = 0;
+                                foreach (string cell in row.Split(','))
+                                {
+                                    if (y == 0)
+                                    {
+                                        Club = cell.Trim();
+                                        y++;
+                                    }
+                                    else if (y == 1)
+                                    {
+                                        LName = cell.Trim();
+                                        y++;
+                                    }
+                                    else if (y == 2)
+                                    {
+                                        Name = cell.Trim();
+                                        y++;
+                                    }
+                                    else if (y == 3)
+                                    {
+                                        Position = cell.Trim();
+                                        y++;
+                                    }
+                                    else if (y == 4)
+                                    {
+                                        Salary = Convert.ToDecimal(cell.Trim());
+                                        y++;
+                                    }
+                                    else
+                                    {
+                                        Compensation = Convert.ToDecimal(cell.Trim());
+                                        var newPlayer = new jugador
+                                        {
+                                            club = Club,
+                                            apellido = LName,
+                                            nombre = Name,
+                                            posicion = Position,
+                                            salario = Salary,
+                                            compesation = Compensation,
+                                            id = i++
+                                        };
+                                        Singleton.Instance1.PlayerDList.Push(newPlayer);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                //stopWatch.Stop();
+                //log += "[CSV Upload] - " + Convert.ToString(stopWatch.Elapsed) + '\n';
+                return View(Singleton._instance1.PlayerDList);
+            }
+            //stopWatch.Stop();
+            return View(Singleton._instance1.PlayerDList);
+        }
+    }
+}
---
+++ Models/Singleton.cs
@@ -0,0 +1,52 @@
+﻿using System.Numerics;
+using System.Collections.Generic;
+using ELineales;
+namespace LAB01_ED1_G.Models.Data
+{
+    public sealed class Singleton
+    {
+        public readonly static Singleton _instance = new Singleton();
+        public System.Collections.Generic.List<jugador> PlayerList;
+        public readonly static Singleton _instance1 = new Singleton();
+        public ELineales.DoubleList<jugador> PlayerDList;
+        public readonly static Singleton _instance2 = new Singleton();
+        public System.Collections.Generic.List<jugador> PlayerSearch;
+        public readonly static Singleton _instance3 = new Singleton();
+        public ELineales.DoubleList<jugador> PlayerDSearch;
+        private Singleton()
+        {
+            PlayerList = new System.Collections.Generic.List<jugador>();
+            PlayerDList = new ELineales.DoubleList<jugador>();
+            PlayerSearch = new System.Collections.Generic.List<jugador>();
+            PlayerDSearch = new ELineales.DoubleList<jugador>();
+        }
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        public static Singleton Instance1
+        {
+            get
+            {
+                return _instance1;
+            }
+        }
+        public static Singleton Instance2
+        {
+            get
+            {
+                return _instance2;
+            }
+        }
+        public static Singleton Instance3
+        {
+            get
+            {
+                return _instance3;
+            }
+        }
+    }
+}
